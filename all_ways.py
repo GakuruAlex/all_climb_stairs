2cc87b1c505ebc2c2e8eeed80ae5f1ccd50ea027
= <=>
2025-03-10 20:40:04 +0300

    use naive recursion to find all possible ways up the stairs given a steps constraint i.e how many stairs you can walk at a time

--- all_ways.py
+++ all_ways.py
@@ -9,3 +9,27 @@ def all_ways_up(stairs: int, steps: List[int]) -> List[List[int]]:
     Returns:
         List[List[int]]: _A list of all the ways to get to the top_
     """
+    all_ways: List[int] = []
+    if stairs < 0:
+        return None
+    if stairs == 0:
+        return [[]]
+    for step in steps:
+        new_stairs: int = stairs - step
+
+        result: List[int]= all_ways_up(stairs= new_stairs, steps= steps)
+        if result != None:
+            for value in result:
+                value.append(step)
+            all_ways.extend(result)
+    return all_ways
+
+def main() -> None:
+    steps: List[int] = [2, 1]
+    stairs: int = 5
+    all: List[List[int]] = all_ways_up(stairs= stairs, steps= steps)
+
+    print(f"All the possible ways to ascend to the top of {stairs} stairs given the steps {steps} are: {all}")
+
+if __name__ == "__main__":
+    main()
