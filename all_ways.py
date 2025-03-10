a558e4ecf64d9d4eeed2e98056afaf64d2116372
= <=>
2025-03-10 18:46:39 +0300

    docstr

---
+++ all_ways.py
@@ -0,0 +1,11 @@
+from typing import List
+def all_ways_up(stairs: int, steps: List[int]) -> List[List[int]]:
+    """_Find all the ways up a given number of stairs and given steps to take each time_
+
+    Args:
+        stairs (int): _Number of stairs_
+        steps (List[int]): _steps you are allowed to take at a time_
+
+    Returns:
+        List[List[int]]: _A list of all the ways to get to the top_
+    """
